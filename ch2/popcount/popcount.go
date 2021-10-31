3d5001308e014b7d820c6d9f764bf68467d88292
com_gunzi <nolispnolife@gmail.com>
2021-10-31 21:34:49 +0900

    fix popcount

--- ch2/popcount/popcount.go
+++ ch2/popcount/popcount.go
@@ -12,21 +12,24 @@ func init() {
 // popCount returns the population count of x
 func PopCount(x uint64) int {
 
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+
+}
+
+// popCount returns the population count of x
+func PopCount23(x uint64) int {
+
 	var tmp int
 	for i, _ := range pc {
 		tmp += int(pc[byte(x>>(i*8))])
 	}
 
 	return tmp
-
-	/*
-		return int(pc[byte(x>>(0*8))] +
-			pc[byte(x>>(1*8))] +
-			pc[byte(x>>(2*8))] +
-			pc[byte(x>>(3*8))] +
-			pc[byte(x>>(4*8))] +
-			pc[byte(x>>(5*8))] +
-			pc[byte(x>>(6*8))] +
-			pc[byte(x>>(7*8))])
-	*/
 }
--- ch2/popcount/popcount_test.go
+++ ch2/popcount/popcount_test.go
@@ -3,5 +3,9 @@ package popcount
 import "testing"
 
 func BenchmarkPopCount(b *testing.B) {
-	PopCount(1)
+	PopCount(18446744073709551615)
+}
+
+func BenchmarkPopCount23(b *testing.B) {
+	PopCount(18446744073709551615)
 }
